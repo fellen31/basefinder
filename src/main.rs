ee839fc0cddb301ee92a7e7ef898a5b5bc4cd92d
Felix Lenner <felixlenner@gmail.com>
2022-02-04 16:51:31 +0100

    I think working version overall now.

--- src/main.rs
+++ src/main.rs
@@ -1,169 +1,181 @@
 extern crate seq_io;
-
-use std::env;
+extern crate noodles;
 use std::str;
-
+use regex;
 use std::fs::File;
 use std::io::{prelude::*, BufReader};
 use std::path::Path;
+use std::io::{self, Write};
 
 use seq_io::fasta::{Reader,Record};
+use lazy_static::lazy_static;
 
-use std::process;
-use std::collections::HashMap;
+use clap::{Parser};
 
-#[derive(Debug)]
+// VCF
+use noodles::vcf as vcf;
 
-struct Person {
+#[derive(Parser,Debug)]
+#[clap(name = "basefinder")]
+#[clap(version = "0.0.1")]
+#[clap(about = "Finds corresponding base in closest species", long_about = None)]
+
+struct Cli {
+    /// Sequence alignment in fasta format
+    #[clap(short,long)]
+    alignment: String,
+    /// #CHROM POS ALT REF HGVS.c STRAND
+    #[clap(short, long)]
+    vcf: String,
+    /// tab-separated distance matrix (id in leftmost column only, not on top)
+    /// Ex: rapidnj <fasta.fa> -o m | tail -n +2 | tr ' ' '\t'
+    #[clap(short, long)]
+    distance_matrix: String,
+    /// hide disregarded
+    #[clap(short, long)]
+    no_disregarded: bool,
+     /// Clean output (hide some columns)
+    #[clap(short, long)]
+    clean_output: bool,
+    /// Hide non-cds
+    #[clap(short, long)]
+    remove_utr: bool,
+}
+struct distance_struct {
     name: String,
-    age: f32
+    distance: f32
 }
 
 fn main() {
+    // Parse command line arguments
+    let cli = Cli::parse(); 
+    // Read files
+    let mut fasta_reader = Reader::from_path(&cli.alignment).unwrap();
+    let mut vcf_reader = csv::ReaderBuilder::new().has_headers(false).delimiter(b'\t').from_reader(File::open(&cli.vcf).unwrap());
+    let mut matrix_reader = csv::ReaderBuilder::new().has_headers(false).delimiter(b'\t').from_reader(File::open(&cli.distance_matrix).unwrap());
+
+    // Collect the results
+    let records: Result<Vec<_>, _> = fasta_reader.records().collect();
+    let csv_records: Result<Vec<csv::StringRecord>, _> = vcf_reader.records().collect();
+    let matrix: Result<Vec<csv::StringRecord>, _> = matrix_reader.records().collect();
     
-    let args: Vec<String> = env::args().collect();
-
-    let mut reader = Reader::from_path(&args[1]).unwrap();
-
-    let mut ranking: Vec<String>;
-    
-    let lines = lines_from_file(&args[2]);
-    
-//    let positions = lines_from_file(&args[3]);
-
-    let mut rdr = csv::ReaderBuilder::new().has_headers(false).delimiter(b'\t').from_reader(File::open(&args[3]).unwrap());
-    let mut rdr_matrix = csv::ReaderBuilder::new().has_headers(false).delimiter(b'\t').from_reader(File::open(&args[4]).unwrap());
-
-    let csv_records: Result<Vec<csv::StringRecord>, _> = rdr.records().collect();
-    let matrix: Result<Vec<csv::StringRecord>, _> = rdr_matrix.records().collect();
-    
-
-    // This is the slow way, might actually work the fast way now
-    let records: Result<Vec<_>, _> = reader.records().collect();
-    
-    let length = records.as_ref().unwrap().iter().len();
-
-            let mut outgroup_rank = 0;
-            let mut outgroup_id = 0;
+    // Initialise some variables
     let mut i = 0;
+    if cli.clean_output {
+
+    writeln!(io::stdout(), "{0: <10}\t{1: <5}\t{2: <1}\t{3: <5}\t{4: <20}\t{5: <5}\t{6: <5}\t{7: <5}\t{8: <20}\t{9: <5}", "VCF_CHR", "VCF_POS", "VCF_REF", "VCF_ALT", "IN_SPC", "IN_NUC", "REF_COR", "OUT_NUC", "OUT_SPC", "OUT_RNK");
+    } else {
+    // Print header
+    writeln!(io::stdout(), "{0: <10}\t{1: <5}\t{2: <1}\t{3: <5}\t{4: <20}\t{5: <5}\t{6: <20}\t{7: <5}\t{8: <5}\t{9: <5}\t{10: <5}\t{11: <5}\t{12: <5}\t{13: <5}\t{14: <5}\t{15: <5}\t{16: <5}\t{17: <20}\t{18: <5}", "VCF_CHR", "VCF_POS", "VCF_REF", "VCF_ALT", "VCF_ANN", "GFF_STR", "IN_SPC", "IN_NUC", "REF!=IN", "REF_COR", "ALT_COR", "ALI_POS", "IN_POS", "GAP", "OUT_NUC", "DSRGD", "NOT_CDS", "OUT_SPC", "OUT_RNK");
+    }
     
-    //let mut outgroup_id = 0;
-    // Check if highest ranking outgroup is present
-    // So for each line
-   // '_outer: for line in lines {
-        // Go through each record 
-        
-     //   let mut records_id = 0;
-
-       // '_inner: for record in records.as_ref().unwrap().iter() {
-            // Save the current record 
-         //   let current_record = str::from_utf8(record.head()).unwrap();
-            // And if the current record matches the line 
-           // if current_record.contains(&line) {
-                //println!("{}", current_record);
-                //println!("{}", line);
-             //   i += 1;
-               // outgroup_id = records_id;
-               // break '_outer;
-
-           // }
-            //records_id += 1;
-       // }
-    //}
-    //if i == 0 {
-      //  eprintln!("{}", "no outgroup found!");
-       // process::exit(1);
-    //}
-    //println!("{}", outgroup_id);
-
-    println!("{0: <18}\t{1: <5}\t{2: <5}\t{3: <5}\t{4: <18}\t{5: <5}\t{6: <5}\t{7: <5}\t{8: <5}\t{9: <5}\t{10: <18}", "VCF_SPC", "VCF_POS", "VCF_REF", "VCF_ALT", "IN_SPC", "IN_NUC", "ALI_POS", "IN_POS", "GAP", "OUT_NUC", "OUT_SPC");
-    let mut species_record: Vec<&str> = Vec::with_capacity(24);
+    // Get which species are present in distance matrix 
+    let n_matrix = matrix.as_ref().unwrap().iter().len();
+    let mut species_record: Vec<&str> = Vec::with_capacity(n_matrix);
+           //     lazy_static! { 
+           //         static ref RE: regex::Regex = regex::Regex::new(r"\d+").unwrap();
+           //     }
+    
     for ma in matrix.as_ref().unwrap() {
         species_record.push(&ma[0].as_ref());
     }
+
     // For every FASTA record
     for record in records.as_ref().unwrap().iter() {
-        let current_record = str::from_utf8(record.head()).unwrap();
+        // Save the name 
+    let mut outgroup_rank = 0;
+    let mut outgroup_id = 0;
+        let mut current_record = str::from_utf8(record.head()).unwrap();
         
-        // Här någonstans välja rätt 
-        
-    // x
-    
-    // Go through every matrix row
-    for ma in matrix.as_ref().unwrap() {
-        //Match the FASTA sequece with the Matrix 
+        // Go through every matrix row
+        // Since we onl
+        for line in matrix.as_ref().unwrap() {
+            
             let mut test: Vec<&String> = Vec::new();
             let mut x = Vec::new();
-            if current_record.contains(&ma[0]) {
-
-                //println!("{} {}", current_record, &ma[0]);
             
+            // Match the FASTA sequece contains the distance matrix species
+            
+            if current_record.contains(&line[0]) {
                 let mut i = 0;
-            // Go through all columns
+            
+                // Then go through all columns
                 for qa in matrix.as_ref().unwrap() {
-                
-                    if i <= matrix.as_ref().unwrap().len() {
+                    
+                    // Skip the first and don't go further than the last
+                    if i <= n_matrix {
                        if i > 1 {
-                           let number = &ma[i];
-                           //println!("{} {}", number, &species_record[i-1]);
-                           let some = Person {name: (&species_record[i-1]).to_string(), age: number.parse::<f32>().unwrap()};
+                           // Get the value at current column 
+                           let number = &line[i];
+                           // Put it into a vector 
+                           let some = distance_struct {name: (&species_record[i-1]).to_string(), distance: number.parse::<f32>().unwrap()};
                            x.push(some);
                        }
                     }
-            
                     i += 1;
                 }
-
-                x.sort_by(|a, b| a.age.partial_cmp(&b.age).unwrap());
-                //println!("Current: {} {:?}", &ma[0], x);
+                // Sort the vector by distance
+                x.sort_by(|a, b| a.distance.partial_cmp(&b.distance).unwrap());
                 let mut i = 0;
                 for element in &x {
                     if i > 0 {
-                        //let string = String:
-                        //println!("{} {}", current_record, element.name);
-                        //if current_record.contains(&element.name) {
                             test.push(&element.name);
-                        //}
                     }
                     i += 1; 
                 } 
-                //println!("Current: {} {:?}", &ma[0], test);
-                //println!("{:?}", test);
-        
-                //println!("{:?}", species);
-            } // If the current fasta record contains
-            '_outer: for ex in test {
+            } 
 
-                //println!("{}", ex);
-            let mut i = 0;
-            '_inner: for record in records.as_ref().unwrap().iter() {
-                let head = str::from_utf8(record.head()).unwrap();
-                if head.contains(ex) {
-                    outgroup_id = i;
-                    //println!("{} {}", head, ex);
-                    break '_outer;
-                }
+            // When distance record matches fasta record, break the loop 
+            // and get the id of the fasta record
+            '_outer: for ex in test {
+                let mut i = 0;
+                    '_inner: for record in records.as_ref().unwrap().iter() {
+                        let head = str::from_utf8(record.head()).unwrap();
+                        if head.contains(ex) {
+                            outgroup_id = i;
+                            break '_outer;
+                } 
                 i += 1;
             }    
             outgroup_rank += 1;
-        }
-        
+        } 
     }
     
 
-        let outgroup_record = str::from_utf8(records.as_ref().unwrap()[outgroup_id].head()).unwrap();
-        
-        if current_record != outgroup_record {
+    let outgroup_record = str::from_utf8(records.as_ref().unwrap()[outgroup_id].head()).unwrap();      // So now we go through every record in the VCF file
 
-            let current_sequence = record.seq();
-            //println!("{:?}", str::from_utf8(current_sequence));
-            let outgroup_sequence = records.as_ref().unwrap()[outgroup_id].seq();
-            //println!("{:?}", str::from_utf8(outgroup_sequence));
+    for recordx in csv_records.as_ref().unwrap() {
+        
+        let chrom = &recordx[0];
+        
+        // Check first so no need to go through everything
+
+        if current_record.contains(chrom) {
+            // Get fields 
+            let pos = &recordx[1];
+            let reference = &recordx[2];
+            let alt = &recordx[3];
+            let hgvs = &recordx[4];
+            let actual_strand = &recordx[5];
+
+            let parsed_position = pos.trim().parse::<i32>().expect("Numeric!");
+            // And if the current FASTA record contains the VCF Record 
+            // Not sure which way to go here, might be too loose naming
+            // The VCF has to be more spefic than the distance matrix
+            // And it should be == to the fasta.
+            // However if fasta has eval|eval_4808_1 etc... 
+            // Best if they are equal and _not_ using contains
+            // Then compare the sequences
             
+            let current_sequence = record.seq();
+            let outgroup_sequence = records.as_ref().unwrap()[outgroup_id].seq();
+            
+
             let mut gap = 0;
             let mut nucleotide = 0;
             let mut position = 0;
             
+            // Go through every base in alignment...can this go on the outside of csv-loop?
             for (base_c, base_o) in current_sequence.iter().zip(outgroup_sequence.iter()) {
                 if base_c == &b'-' {
                     gap += 1;
@@ -171,35 +183,85 @@ fn main() {
                     nucleotide += 1;
                 }
 
-            position += 1;
-            
-            for recordx in csv_records.as_ref().unwrap() {
-                let chrom = &recordx[0];
-                let pos = &recordx[1];
-                let reference = &recordx[2];
-                let alt = &recordx[3];
-                let strand = &recordx[4];
-
-                let temp = pos.trim().parse::<i32>().expect("Numeric!");
-                if current_record.contains(&chrom) {
-                    if nucleotide == temp {
-                        //println!("{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}", *base_c as char, nucleotide, gap, position, *base_o as char, temp, chrom, outgroup_record);
-                        println!("{0: <18}\t{1: <5}\t{2: <5}\t{3: <5}\t{4: <18}\t{5: <5}\t{6: <5}\t{7: <5}\t{8: <5}\t{9: <5}\t{10: <18}\t{11: <5}", chrom, pos, reference, alt, current_record, *base_c as char, position, nucleotide, gap, *base_o as char, outgroup_record, outgroup_rank);
+                position += 1;
                 
-                    }
-                } 
-            }
+                
+                let mut disregard = 0; 
+                let mut not_cds = 0; 
+                
+                let bref = reference.as_bytes()[0];
+                let balt = alt.as_bytes()[0];
 
-            }
+                let mut strand_corrected = &bref;
+                let mut strand_correctedalt = &balt;
+
+                let ss = &hgvs.to_string();
+                    
+                if ss.contains("-") || ss.contains("*") {
+                    not_cds += 1;
+                }
+
+                let corr_pos = ss.chars().find(|a| a.is_digit(10)).and_then(|a| a.to_digit(10)).unwrap();
+                
+                //let cap = RE.captures(ss).unwrap();
+                let mut base_vs_reference = 0;
+
+                  //  let corr_pos = cap.get(0).map_or("", |m| m.as_str()).parse::<i32>().unwrap();
+                //if nucleotide == parsed_position {
+                // Not sure if the && ... part is ok..
+                if nucleotide == corr_pos && *base_c != b'-' {
+
+                    if *base_c != reference.as_bytes()[0] {
+                        base_vs_reference = 1;
+                    
+                        if actual_strand.as_bytes()[0] == b'-' {
+                            match bref {
+                                b'A' => strand_corrected = &&b'T',
+                                b'C' => strand_corrected = &&b'G',
+                                b'G' => strand_corrected = &&b'C',
+                                b'T' => strand_corrected = &&b'A',
+                                _ => panic!("panik!"),
+                            } 
+                            match balt {
+                                b'A' => strand_correctedalt = &&b'T',
+                                b'C' => strand_correctedalt = &&b'G',
+                                b'G' => strand_correctedalt = &&b'C',
+                                b'T' => strand_correctedalt = &&b'A',
+                                _ => panic!("panik!"),
+                            }
+                        }
+                    
+                      } 
+                    
+                    if strand_corrected != base_o && strand_correctedalt != base_o {
+                        disregard += 1;
+                    }
+                                    
+                    if cli.no_disregarded {
+                        if disregard > 0 {
+                            continue;
+                        }
+                    }
+
+                    if cli.remove_utr {
+                    if not_cds > 0 {
+                            continue;
+                        }
+                    }
+                    if cli.clean_output {
+
+                    writeln!(io::stdout(), "{0: <10}\t{1: <5}\t{2: <1}\t{3: <5}\t{4: <20}\t{5: <5}\t{6: <5}\t{7: <5}\t{8: <20}\t{9: <5}", chrom, pos, reference, alt, current_record, *base_c as char, *strand_corrected, *base_o as char, outgroup_record, outgroup_rank);
+                    } else {
+                    
+                    writeln!(io::stdout(), "{0: <10}\t{1: <5}\t{2: <5}\t{3: <5}\t{4: <20}\t{5: <5}\t{6: <20}\t{7: <5}\t{8: <5}\t{9: <5}\t{10: <5}\t{11: <5}\t{12: <5}\t{13: <5}\t{14: <5}\t{15: <5}\t{16: <5}\t{17: <20}\t{18: <5}", chrom, pos, reference, alt, hgvs.to_string(), actual_strand, current_record, *base_c as char, base_vs_reference,*strand_corrected as char, *strand_correctedalt as char, position, nucleotide, gap, *base_o as char, disregard, not_cds, outgroup_record, outgroup_rank);
+                 //   if reference.as_bytes()[0] != *base_c {
+                 //       panic!("Reference: {} does not match ingroup base: {}!", reference.as_bytes()[0], base_c);
+                 //   }
+                }
+                    }
+            } 
+
         }
     }
-    
 }
-
-fn lines_from_file(filename: impl AsRef<Path>) -> Vec<String> {
-    let file = File::open(filename).expect("no such file");
-    let buf = BufReader::new(file);
-    buf.lines()
-        .map(|l| l.expect("Could not parse line"))
-        .collect()
 }
